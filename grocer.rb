913772ed7f8b45e773e4cc65d4eed44fc88c3937
Juha Penttilä <juha.ea.penttila@gmail.com>
2020-03-05 02:35:14 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -90,7 +90,7 @@ final_cart = apply_clearance(couponed_cart)
 total = 0 
 counter = 0 
 while counter < final_cart.length
-total += final_cart[counter][:price] * final_cart
+total += final_cart[counter][:price] * final_cart[counter]
 
 counter +=1 
 end
