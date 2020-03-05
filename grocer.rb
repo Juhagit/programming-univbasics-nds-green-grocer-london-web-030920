ad87811903e38a53a4f6889478160918dc6bea8d
Juha Penttilä <juha.ea.penttila@gmail.com>
2020-03-05 02:14:34 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -68,9 +68,10 @@ end
 end
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+ 
+ 
+ 
+ 
 end
 
 def checkout(cart, coupons)
